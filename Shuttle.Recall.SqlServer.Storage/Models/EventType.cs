780c8f9b8508cf0fd247c199c1e6d2b4786676f3
Eben <me@ebenroux.co.za>
2025-12-27 19:56:26 +0200

    Removed models.

--- Shuttle.Recall.SqlServer.Storage/Models/EventType.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Shuttle.Recall.SqlServer.Storage.Models;
-
-public class EventType
-{
-    public Guid Id { get; set; }
-    public string TypeName { get; set; } = null!;
-}
--- Shuttle.Recall.SqlServer.Storage/Models/IdKey.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Shuttle.Recall.SqlServer.Storage.Models;
-
-public class IdKey
-{
-    public string UniqueKey { get; set; } = null!;
-    public Guid Id { get; set; }
-}
