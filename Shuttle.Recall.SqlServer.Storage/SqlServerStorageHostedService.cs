59b451f5de5b1786a6cd455a6bce1ca0228b965f
Eben <me@ebenroux.co.za>
2026-01-02 10:37:15 +0200

    Create tables only; else use upgrade.

--- Shuttle.Recall.SqlServer.Storage/SqlServerStorageHostedService.cs
+++ Shuttle.Recall.SqlServer.Storage/SqlServerStorageHostedService.cs
@@ -27,7 +27,7 @@ public class SqlServerStorageHostedService(IOptions<SqlServerStorageOptions> sql
             try
             {
                 await using var dbContext = await _dbContextFactory.CreateDbContextAsync(cancellationToken);
-                
+
                 await dbContext.Database.ExecuteSqlRawAsync($@"
 DECLARE @lock_result INT;
 EXEC @lock_result = sp_getapplock @Resource = '{typeof(SqlServerStorageHostedService).FullName}', @LockMode = 'Exclusive', @LockOwner = 'Session', @LockTimeout = 15000;
@@ -48,7 +48,9 @@ BEGIN TRY
             [Id] [uniqueidentifier] NOT NULL,
             [TypeName] [nvarchar](1024) NOT NULL,
             CONSTRAINT [PK_EventType] PRIMARY KEY CLUSTERED ([Id] ASC)
-        ) ON [PRIMARY]
+        ) ON [PRIMARY];
+
+        CREATE UNIQUE NONCLUSTERED INDEX [IX_EventType] ON [{_sqlServerStorageOptions.Schema}].[EventType] ([TypeName] ASC);
     END
 
     IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[IdKey]') AND type in (N'U'))
@@ -58,7 +60,7 @@ BEGIN TRY
             [UniqueKey] [nvarchar](450) NOT NULL,
             [Id] [uniqueidentifier] NOT NULL,
             CONSTRAINT [PK_IdKey] PRIMARY KEY CLUSTERED ([UniqueKey] ASC)
-        ) ON [PRIMARY]
+        ) ON [PRIMARY];
     END
 
     IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[PrimitiveEvent]') AND type in (N'U'))
@@ -71,86 +73,102 @@ BEGIN TRY
             [EventEnvelope] [varbinary](max) NOT NULL,
             [EventId] [uniqueidentifier] NOT NULL,
             [EventTypeId] [uniqueidentifier] NOT NULL,
-            [DateRegistered] [datetime2](7) NOT NULL,
+            [RecordedAt] [datetimeoffset](7) NOT NULL,
             [SequenceNumber] [bigint] NULL,
             CONSTRAINT [PK_PrimitiveEvent] PRIMARY KEY CLUSTERED ([Id] ASC, [Version] ASC)
-        ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-    END
+        ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY];
 
-    IF EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[PrimitiveEvent]') AND name = 'SequenceNumber')
+        ALTER TABLE [{_sqlServerStorageOptions.Schema}].[PrimitiveEvent]
+            ADD CONSTRAINT [DF_PrimitiveEvent_RecordedAt] DEFAULT (SYSUTCDATETIME()) FOR [RecordedAt];
+
+        ALTER TABLE [{_sqlServerStorageOptions.Schema}].[PrimitiveEvent] 
+            WITH CHECK ADD CONSTRAINT [FK_PrimitiveEvent_EventType_EventTypeId] 
+            FOREIGN KEY([EventTypeId]) REFERENCES [{_sqlServerStorageOptions.Schema}].[EventType] ([Id]) ON DELETE CASCADE;
+
+        ALTER TABLE [{_sqlServerStorageOptions.Schema}].[PrimitiveEvent] 
+            CHECK CONSTRAINT [FK_PrimitiveEvent_EventType_EventTypeId];
+
+        CREATE NONCLUSTERED INDEX [IX_PrimitiveEvent_EventTypeId] 
+            ON [{_sqlServerStorageOptions.Schema}].[PrimitiveEvent] ([EventTypeId] ASC);
+
+        CREATE UNIQUE NONCLUSTERED INDEX [IX_PrimitiveEvent_SequenceNumber] 
+            ON [{_sqlServerStorageOptions.Schema}].[PrimitiveEvent] ([SequenceNumber] ASC, [RecordedAt] ASC)
+            WHERE [SequenceNumber] IS NOT NULL;
+
+        CREATE NONCLUSTERED INDEX [IX_PrimitiveEvent_NullSequence_DateRegistered_Version] 
+            ON [{_sqlServerStorageOptions.Schema}].[PrimitiveEvent] ([RecordedAt] ASC, [Version] ASC)
+            WHERE [SequenceNumber] IS NULL;
+    END
+    ELSE
     BEGIN
-        IF COLUMNPROPERTY(OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[PrimitiveEvent]'), 'SequenceNumber', 'IsIdentity') = 1
+        DECLARE @needsUpgrade BIT = 0;
+
+        IF EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[PrimitiveEvent]') AND name = 'SequenceNumber')
         BEGIN
-            ALTER TABLE [{_sqlServerStorageOptions.Schema}].[PrimitiveEvent] ADD [SequenceNumber_Temp] BIGINT NULL;
-            EXEC('UPDATE [{_sqlServerStorageOptions.Schema}].[PrimitiveEvent] SET [SequenceNumber_Temp] = [SequenceNumber]');
-            ALTER TABLE [{_sqlServerStorageOptions.Schema}].[PrimitiveEvent] DROP COLUMN [SequenceNumber];
-            EXEC sp_rename '[{_sqlServerStorageOptions.Schema}].[PrimitiveEvent].[SequenceNumber_Temp]', 'SequenceNumber', 'COLUMN';
-        END
-        ELSE
-        BEGIN
-            IF EXISTS (
-                SELECT 1 
-                FROM INFORMATION_SCHEMA.COLUMNS 
-                WHERE TABLE_SCHEMA = '{_sqlServerStorageOptions.Schema}'
-                  AND TABLE_NAME = 'PrimitiveEvent'
-                  AND COLUMN_NAME = 'SequenceNumber'
-                  AND IS_NULLABLE = 'NO'
-            )
+            IF COLUMNPROPERTY(OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[PrimitiveEvent]'), 'SequenceNumber', 'IsIdentity') = 1
             BEGIN
-                ALTER TABLE [{_sqlServerStorageOptions.Schema}].[PrimitiveEvent]                 ALTER COLUMN [SequenceNumber] BIGINT NULL;
+                SET @needsUpgrade = 1;
             END
         END
+
+        IF EXISTS (
+            SELECT 1 
+            FROM INFORMATION_SCHEMA.COLUMNS 
+            WHERE TABLE_SCHEMA = '{_sqlServerStorageOptions.Schema}'
+              AND TABLE_NAME = 'PrimitiveEvent'
+              AND COLUMN_NAME = 'SequenceNumber'
+              AND IS_NULLABLE = 'NO'
+        )
+        BEGIN
+            SET @needsUpgrade = 1;
+        END
+
+        IF EXISTS (
+            SELECT 1
+            FROM sys.columns
+            WHERE object_id = OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[PrimitiveEvent]')
+              AND name = 'DateRegistered'
+        )
+        BEGIN
+            SET @needsUpgrade = 1;
+        END
+
+        IF EXISTS (
+            SELECT * FROM sys.columns 
+            WHERE object_id = OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[PrimitiveEvent]') 
+            AND name = 'DateCommitted'
+        )
+        BEGIN
+            SET @needsUpgrade = 1;
+        END
+
+        IF EXISTS (
+            SELECT * FROM sys.indexes 
+            WHERE name = N'IX_PrimitiveEvent_DateCommitted_Filtered_Null' 
+            AND object_id = OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[PrimitiveEvent]')
+        )
+        BEGIN
+            SET @needsUpgrade = 1;
+        END
+
+        IF EXISTS (
+            SELECT 1 
+            FROM INFORMATION_SCHEMA.COLUMNS 
+            WHERE TABLE_SCHEMA = '{_sqlServerStorageOptions.Schema}'
+              AND TABLE_NAME = 'PrimitiveEvent'
+              AND COLUMN_NAME = 'RecordedAt'
+              AND DATA_TYPE = 'datetime2'
+        )
+        BEGIN
+            SET @needsUpgrade = 1;
+        END
+
+        IF @needsUpgrade = 1
+        BEGIN
+            THROW 50001, 'Database schema is outdated. Please upgrade the database to version 21.0.0 using the Shuttle.Recall.SqlServer.Storage.Upgrade application.', 1;
+        END
     END
 
-    IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[EventType]') AND name = N'IX_EventType')
-    BEGIN
-        CREATE UNIQUE NONCLUSTERED INDEX [IX_EventType] ON [{_sqlServerStorageOptions.Schema}].[EventType] ([TypeName] ASC);
-    END
-
-    IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = N'IX_PrimitiveEvent_SequenceNumber' AND object_id = OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[PrimitiveEvent]'))
-    BEGIN
-        CREATE UNIQUE NONCLUSTERED INDEX [IX_PrimitiveEvent_SequenceNumber] 
-        ON [{_sqlServerStorageOptions.Schema}].[PrimitiveEvent] ([SequenceNumber] ASC, [DateRegistered] ASC)
-        WHERE [SequenceNumber] IS NOT NULL;
-    END
-
-    IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = N'IX_PrimitiveEvent_NullSequence_DateRegistered_Version' AND object_id = OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[PrimitiveEvent]'))
-    BEGIN
-        CREATE NONCLUSTERED INDEX [IX_PrimitiveEvent_NullSequence_DateRegistered_Version] 
-        ON [{_sqlServerStorageOptions.Schema}].[PrimitiveEvent] ([DateRegistered] ASC, [Version] ASC)
-        WHERE [SequenceNumber] IS NULL;
-    END
-
-    IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[PrimitiveEvent]') AND name = N'IX_PrimitiveEvent_EventTypeId')
-    BEGIN
-        CREATE NONCLUSTERED INDEX [IX_PrimitiveEvent_EventTypeId] ON [{_sqlServerStorageOptions.Schema}].[PrimitiveEvent] ([EventTypeId] ASC);
-    END
-
-    IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[DF_PrimitiveEvent_DateRegistered]') AND type = 'D')
-    BEGIN
-        ALTER TABLE [{_sqlServerStorageOptions.Schema}].[PrimitiveEvent] ADD CONSTRAINT DF_PrimitiveEvent_DateRegistered DEFAULT (GETUTCDATE()) FOR [DateRegistered]
-    END
-
-    IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[FK_PrimitiveEvent_EventType_EventTypeId]') AND parent_object_id = OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[PrimitiveEvent]'))
-    BEGIN
-        ALTER TABLE [{_sqlServerStorageOptions.Schema}].[PrimitiveEvent] WITH CHECK ADD CONSTRAINT [FK_PrimitiveEvent_EventType_EventTypeId] FOREIGN KEY([EventTypeId]) REFERENCES [{_sqlServerStorageOptions.Schema}].[EventType] ([Id]) ON DELETE CASCADE
-    END
-
-    IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[FK_PrimitiveEvent_EventType_EventTypeId]') AND parent_object_id = OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[PrimitiveEvent]'))
-    BEGIN
-        ALTER TABLE [{_sqlServerStorageOptions.Schema}].[PrimitiveEvent] CHECK CONSTRAINT [FK_PrimitiveEvent_EventType_EventTypeId]
-    END
-
-    /* DROP */
-    IF EXISTS (SELECT * FROM sys.indexes WHERE name = N'IX_PrimitiveEvent_DateCommitted_Filtered_Null' AND object_id = OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[PrimitiveEvent]'))
-    BEGIN
-        DROP INDEX [IX_PrimitiveEvent_DateCommitted_Filtered_Null] ON [{_sqlServerStorageOptions.Schema}].[PrimitiveEvent];
-    END
-
-    IF EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'[{_sqlServerStorageOptions.Schema}].[PrimitiveEvent]') AND name = 'DateCommitted')
-    BEGIN
-        ALTER TABLE [{_sqlServerStorageOptions.Schema}].[PrimitiveEvent] DROP COLUMN [DateCommitted];
-    END
 END TRY
 BEGIN CATCH
     EXEC sp_releaseapplock @Resource = '{typeof(SqlServerStorageHostedService).FullName}', @LockOwner = 'Session';
@@ -165,7 +183,6 @@ EXEC sp_releaseapplock @Resource = '{typeof(SqlServerStorageHostedService).FullN
             catch
             {
                 retryCount++;
-
                 if (retryCount > 3)
                 {
                     throw;
